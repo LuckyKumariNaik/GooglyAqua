ac6140acc089f9bb39b61bb48c354bece8b05c9c
LuckyKumariNaik <luckyofficial418@gmail.com>
2026-06-04 14:17:25 +0530

    add delete reviews route

--- backend/app.py
+++ backend/app.py
@@ -130,12 +130,6 @@ def show_reviews():
     reviews = Review.query.all()
     return str([(r.name, r.rating, r.review_message) for r in reviews])
 
-@app.route('/delete-all-reviews')
-def delete_all_reviews():
-    Review.query.delete()
-    db.session.commit()
-    return "All reviews deleted!"
-
 
 
 with app.app_context():
